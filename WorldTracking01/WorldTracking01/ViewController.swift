45db55b8563668d965933f2fc63ca35da2009d32
Tod Fleeman <rtoddf@me.com>
2018-07-05 12:42:36 -0400

    first arkit project

---
+++ WorldTracking01/WorldTracking01/ViewController.swift
@@ -0,0 +1,28 @@
+import UIKit
+import ARKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var sceneView: ARSCNView!
+    let configuration = ARWorldTrackingConfiguration()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints, ARSCNDebugOptions.showWorldOrigin]
+        self.sceneView.session.run(configuration)
+    }
+
+    @IBAction func add(_ sender: Any) {
+        let node = SCNNode()
+        node.geometry = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0)
+        node.geometry?.firstMaterial?.diffuse.contents = UIColor.orange
+        // numbers are in meters - 1.0 is one meter. 0.3 is 30 centimeters
+        node.position = SCNVector3(0.1,0.2,-0.3)
+        self.sceneView.scene.rootNode.addChildNode(node)
+        
+        
+    }
+    
+}
+

f1db51a450216db9c081c3c2a751ead5cdc29cbc
Todd Fleeman <rtoddf@comcast.net>
2018-07-06 14:04:36 -0400

    adding custom shapes

--- WorldTracking01/WorldTracking01/ViewController.swift
+++ WorldTracking01/WorldTracking01/ViewController.swift
@@ -16,16 +16,29 @@ class ViewController: UIViewController {
 
     @IBAction func add(_ sender: Any) {
         let node = SCNNode()
-        node.geometry = SCNBox(width: 0.05, height: 0.05, length: 0.05, chamferRadius: 0)
+//        node.geometry = SCNBox(width: 0.05, height: 0.05, length: 0.05, chamferRadius: 0)
+//        node.geometry = SCNCapsule(capRadius: 0.1, height: 0.3)
+//        node.geometry = SCNCone(topRadius: 0.0, bottomRadius: 0.3, height: 0.3)
+//        node.geometry = SCNCylinder(radius: 0.2, height: 0.2)
+        
+        let path = UIBezierPath()
+        path.move(to: CGPoint(x: 0, y: 0))
+        path.addLine(to: CGPoint(x: 0, y: 0.2))
+        path.addLine(to: CGPoint(x: 0.2, y: 0.3))
+        path.addLine(to: CGPoint(x: 0.4, y: 0.2))
+        path.addLine(to: CGPoint(x: 0.4, y: 0))
+        
+        node.geometry = SCNShape(path: path, extrusionDepth: 0.2)
+        
         node.geometry?.firstMaterial?.diffuse.contents = UIColor.orange
         node.geometry?.firstMaterial?.specular.contents = UIColor.white
         
-        let x = randomNumbers(firstNum: 0, secondNum: 0.15)
-        let y = randomNumbers(firstNum: 0, secondNum: 0.15)
-        let z = randomNumbers(firstNum: 0, secondNum: -0.3)
+//        let x = randomNumbers(firstNum: 0, secondNum: 0.15)
+//        let y = randomNumbers(firstNum: 0, secondNum: 0.15)
+//        let z = randomNumbers(firstNum: 0, secondNum: -0.3)
         
         // numbers are in meters - 1.0 is one meter. 0.3 is 30 centimeters
-        node.position = SCNVector3(x, y, z)
+        node.position = SCNVector3(0, 0, -0.3)
         self.sceneView.scene.rootNode.addChildNode(node)
     }
     

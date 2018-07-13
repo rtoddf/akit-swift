660a5c137229559e99b097d94e13ff36481c3940
Todd Fleeman <rtoddf@comcast.net>
2018-07-13 12:15:51 -0400

    rotation on its own axis done

--- Planets01/Planets01/Objects.swift
+++ Planets01/Planets01/Objects.swift
@@ -9,6 +9,7 @@ struct planetaryObject {
     var emissionImage:UIImage?
     var normalImage:UIImage?
     var position:SCNVector3
+    var rotationSpeed:Double
     
     func createPlanetaryBody(geometry:SCNGeometry, diffuse:UIImage, specular:UIImage?, emission:UIImage?, normal:UIImage?, position:SCNVector3) -> SCNNode {
         let node = SCNNode()
--- Planets01/Planets01/ViewController.swift
+++ Planets01/Planets01/ViewController.swift
@@ -17,21 +17,30 @@ class ViewController: UIViewController {
     
     override func viewDidAppear(_ animated: Bool) {
 
-        let sun = planetaryObject(name: "sun", geometry: SCNSphere(radius: 0.432), diffuseImage: #imageLiteral(resourceName: "SunDiffuse"), specularImage: nil, emissionImage: nil, normalImage: nil, position: SCNVector3(0,0,-3.0))
+        let sun = planetaryObject(name: "sun", geometry: SCNSphere(radius: 0.432), diffuseImage: #imageLiteral(resourceName: "SunDiffuse"), specularImage: nil, emissionImage: nil, normalImage: nil, position: SCNVector3(0,0,-3.0), rotationSpeed: 8.0)
         let sunNode = createPlanetaryBody(geometry: sun.geometry, diffuse: sun.diffuseImage, specular: sun.specularImage, emission: sun.emissionImage, normal: sun.normalImage, position: sun.position)
         self.sceneView.scene.rootNode.addChildNode(sunNode)
         
-        let action = SCNAction.rotateBy(x: 0, y: CGFloat(360.degreesToRadians), z: 0, duration: 8)
+        let action = SCNAction.rotateBy(x: 0, y: CGFloat(360.degreesToRadians), z: 0, duration: sun.rotationSpeed)
         let forever = SCNAction.repeatForever(action)
         sunNode.runAction(forever)
         
-        let earth = planetaryObject(name: "earth", geometry: SCNSphere(radius: 0.13959), diffuseImage: #imageLiteral(resourceName: "EarthDiffuse"), specularImage: #imageLiteral(resourceName: "EarthSpecular"), emissionImage: #imageLiteral(resourceName: "EarthEmission"), normalImage: #imageLiteral(resourceName: "EarthNormal"), position: SCNVector3(1.2,0,0))
-        let venus = planetaryObject(name: "venus", geometry: SCNSphere(radius: 0.13760), diffuseImage: #imageLiteral(resourceName: "VenusDiffuse"), specularImage: nil, emissionImage: #imageLiteral(resourceName: "VenusSpecular"), normalImage: nil, position: SCNVector3(0.7,0,0))
-        self.planetObjects.append(earth)
+        let venus = planetaryObject(name: "venus", geometry: SCNSphere(radius: 0.13760), diffuseImage: #imageLiteral(resourceName: "VenusDiffuse"), specularImage: nil, emissionImage: #imageLiteral(resourceName: "VenusSpecular"), normalImage: nil, position: SCNVector3(0.7,0,0), rotationSpeed: 1.0)
+        let earth = planetaryObject(name: "earth", geometry: SCNSphere(radius: 0.13959), diffuseImage: #imageLiteral(resourceName: "EarthDiffuse"), specularImage: #imageLiteral(resourceName: "EarthSpecular"), emissionImage: #imageLiteral(resourceName: "EarthEmission"), normalImage: #imageLiteral(resourceName: "EarthNormal"), position: SCNVector3(1.2,0,0), rotationSpeed: 14.0)
         self.planetObjects.append(venus)
+        self.planetObjects.append(earth)
 
-        for planet in self.planetObjects {
-            let planet:SCNNode = createPlanetaryBody(geometry: planet.geometry, diffuse: planet.diffuseImage, specular: planet.specularImage, emission: planet.emissionImage, normal: planet.normalImage, position: planet.position)
+        for body in self.planetObjects {
+            let planet:SCNNode = createPlanetaryBody(geometry: body.geometry, diffuse: body.diffuseImage, specular: body.specularImage, emission: body.emissionImage, normal: body.normalImage, position: body.position)
+            
+            let planetCenterNode = SCNNode()
+            planetCenterNode.position = body.position
+            planet.addChildNode(planetCenterNode)
+            
+            let planetAction = SCNAction.rotateBy(x: 0, y: CGFloat(360.degreesToRadians), z: 0, duration: body.rotationSpeed)
+            let planetForever = SCNAction.repeatForever(planetAction)
+            planet.runAction(planetForever)
+            
             sunNode.addChildNode(planet)
         }
 

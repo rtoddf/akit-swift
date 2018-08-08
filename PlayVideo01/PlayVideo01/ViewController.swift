a72b2e08b034776eaad889382258d3a283a558ea
Tod Fleeman <rtoddf@me.com>
2018-08-08 15:55:43 -0400

    renaming vars for video

--- PlayVideo01/PlayVideo01/ViewController.swift
+++ PlayVideo01/PlayVideo01/ViewController.swift
@@ -58,8 +58,6 @@ class ViewController: UIViewController {
 
         videoNode.size = CGSize(width: (skScene.size.width/2) - (CGFloat(labelMargin) * 2), height: ((skScene.size.width/2) - (CGFloat(labelMargin) * 2)) * 9/16)
         videoNode.position = CGPoint(x: skScene.size.width/2 + videoNode.size.width/2 + CGFloat(labelMargin), y: (skScene.size.height - videoNode.size.height/2) - CGFloat(labelMargin))
-//        videoNode.size = skScene.size
-//        UIColor(white: 0.5, alpha: 0.5)
         skScene.backgroundColor = .clear
         
         
@@ -67,40 +65,31 @@ class ViewController: UIViewController {
         
         let newTitleLabel = SKMultilineLabel(text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sed lacus ornare, ornare sem et, tristique lectus. Vestibulum in fringilla enim. Duis fringilla lectus id placerat porttitor.", labelWidth: Int((skScene.size.width/2) - (CGFloat(labelMargin) * 2)), pos: labelPosition)
         
-//        print("x: \((skScene.size.height/2) - (CGFloat(labelMargin) * 2) - CGFloat(labelMargin))")
-//        print("y: \((skScene.size.height - videoNode.size.height/2) - CGFloat(labelMargin))")
-        
-//        newTitleLabel.labelHeight = labelHeight
         newTitleLabel.alignment = .left
         newTitleLabel.leading = 60
         newTitleLabel.fontColor = .white
-//        newTitleLabel.fontColor = .red
         newTitleLabel.fontSize = 60
         skScene.addChild(newTitleLabel)
         
-//        let titleLabel = SKLabelNode(fontNamed:"HelveticaNeue-Bold")
-//        titleLabel.horizontalAlignmentMode = .right
-//        titleLabel.fontSize = 36
-//        titleLabel.text = String("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sed lacus ornare, ornare sem et, tristique lectus")
-//        titleLabel.fontColor = .white
-//
-//        titleLabel.position = CGPoint(x: skScene.size.width/2, y: skScene.size.height/2)
-//        skScene.addChild(titleLabel)
+        // printing stuff out
+        // print("x: \((skScene.size.height/2) - (CGFloat(labelMargin) * 2) - CGFloat(labelMargin))")
+        // print("y: \((skScene.size.height - videoNode.size.height/2) - CGFloat(labelMargin))")
 
-        let tvPlane = SCNPlane(width: 1.0, height: 0.5265)
-        tvPlane.firstMaterial?.diffuse.contents = skScene
-        tvPlane.firstMaterial?.isDoubleSided = true
+        let videoPlane = SCNPlane(width: 1.0, height: 0.5265)
+        videoPlane.firstMaterial?.diffuse.contents = skScene
+        videoPlane.firstMaterial?.isDoubleSided = true
 
-        let tvPlaneNode = SCNNode(geometry: tvPlane)
+        let videoPlaneNode = SCNNode(geometry: videoPlane)
 
         var translation = matrix_identity_float4x4
         translation.columns.3.z = -1.5
 
-        tvPlaneNode.simdTransform = matrix_multiply(currentFrame.camera.transform, translation)
-        tvPlaneNode.eulerAngles = SCNVector3(Double.pi,0,0)
+        videoPlaneNode.simdTransform = matrix_multiply(currentFrame.camera.transform, translation)
+        videoPlaneNode.eulerAngles = SCNVector3(Double.pi,0,0)
 
-        self.sceneView.scene.rootNode.addChildNode(tvPlaneNode)
+        self.sceneView.scene.rootNode.addChildNode(videoPlaneNode)
     }
     
 }
 
+//        UIColor(white: 0.5, alpha: 0.5)
